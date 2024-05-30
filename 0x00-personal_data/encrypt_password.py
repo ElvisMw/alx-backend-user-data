45d9f96cd6166e007d8c2de513ce856a3dd6273b
ElvisMw <elvismwanthi@gmail.com>
2024-05-30 20:35:38 +0300

    encrypt password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """password validation"""
 
+
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """Validates that the provided password matches the hashed password."""
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
-
