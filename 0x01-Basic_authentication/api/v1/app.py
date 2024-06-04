949cc8889f47e0256dcca6338c9d17799de0f53d
ElvisMw <elvismwanthi@gmail.com>
2024-06-04 21:18:08 +0300

    2. Error handler: Forbidden

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -4,7 +4,7 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
+from flask import Flask, jsonify, abort
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -13,17 +13,21 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler
-    """
+def not_found(error):
+    """ Not found handler """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
-def unauthorized(error) -> str:
-    """ Unauthorized handler
-    """
+def unauthorized(error):
+    """ Unauthorized handler """
     return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error):
+    """ Forbidden handler """
+    return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
