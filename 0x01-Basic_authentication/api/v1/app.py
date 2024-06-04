d18f0797c464f27d52a72f98f892f316f900a209
ElvisMw <elvismwanthi@gmail.com>
2024-06-04 21:05:05 +0300

    1. Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -4,7 +4,7 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request, Response
+from flask import Flask, jsonify, abort, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -13,32 +13,15 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.errorhandler(404)
-def not_found(error) -> Response:
-    """
-    Not found handler.
-
-    Parameters:
-        error (Exception): The exception object representing the not
-        found error
-
-    Returns:
-        Response: A JSON response indicating that the resource was not found
+def not_found(error) -> str:
+    """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
-def unauthorized(error) -> Response:
-    """
-    Handle unauthorized requests by returning a JSON response
-    with an "error" key set to "Unauthorized" and a status code of 401.
-
-    Parameters:
-        error (Exception): The exception object representing the
-        unauthorized request.
-
-    Returns:
-        Response: A JSON response and the status code.
+def unauthorized(error) -> str:
+    """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,33 +1,35 @@
 #!/usr/bin/env python3
 """ Module of Index views
 """
-from flask import jsonify, abort, Response
+from flask import jsonify, abort
 from api.v1.views import app_views
-from typing import Dict
-from models.user import User
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status() -> Response:
-    """
-    GET /api/v1/status
-    Returns the status of the API.
-
+def status() -> str:
+    """ GET /api/v1/status
     Return:
-        - JSON response with the status of the API
+      - the status of the API
     """
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats/', strict_slashes=False)
-def stats() -> Response:
-    """
-    GET /api/v1/stats
-    Returns the number of each object.
-
+def stats() -> str:
+    """ GET /api/v1/stats
     Return:
-        - JSON response with the count of User objects
+      - the number of each objects
     """
-    stats: Dict[str, int] = {}
+    from models.user import User
+    stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - an unauthorized error
+    """
+    abort(401)
